808ebca8d07192feea9da7aacef4ae52aceffcc8
Carole <carole.ambert@lepoles.com>
2017-11-13 17:05:47 +0100

    Add files via upload

---
+++ accueil.php
@@ -0,0 +1,229 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="assets/style/style.css">
+    <link rel="stylesheet" href="assets/style/bons_plans.css">
+    <link rel="stylesheet" href="assets/style/accueil.css">
+   <link rel="stylesheet" href="assets/style/slider.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+</head>
+
+<body>
+
+    <!--<div data-include="header"></div>-->
+    <!--<div data-include="recherche"></div>-->
+    <?php require_once('include/header.html') ?>
+    <?php require_once('include/slider.html') ?>
+    <?php require_once('include/recherche.html') ?>
+
+    <!--Scrollback button-->
+    <button onclick="topFunction()" id="myBtn" title="Go to top"><i class="fa fa-arrow-circle-up" aria-hidden="true"></i></button>
+    <!---->
+
+
+    <!--Slider bons plans-->
+    <h2>Bons plans du moment</h2>
+    
+    <div class="slider">
+   
+    <div class="slider-element slider-element-1 slider-element-actuel" id="slider-element-1" style="background-image: url(assets/img/accueil/jeu.jpg);"></div>
+    
+    <div class="slider-element slider-element-2 slider-element-disabled" id="slider-element-2" style="background-image: url(assets/img/accueil/vignette10.png)"></div>
+    
+        
+    <div class="fleche-middle slide-left">
+        <i class="fa fa-chevron-left"></i>
+    </div>
+    
+    <div class="bullets-centre">
+        <ul class="bullets">
+            <li data-bullet="1" class="bullet bullet-active"></li>
+            <li data-bullet="2" class="bullet"></li>
+            <li data-bullet="3" class="bullet"></li>
+        </ul>
+    </div>
+    
+    <div class="fleche-middle slide-right">
+        <i class="fa fa-chevron-right"></i>
+    </div>
+    
+</div>
+
+    
+    <div class="cartepicto">
+        <a class="bouton_rose" title="Voir tous les bons plans " href="http://www.cartejeunes.fr/nos-bons-plans ">Voir tous les bons plans</a>
+    </div>
+
+    <!--Slider top avantages-->
+
+    <h2>Top Avantages</h2>
+
+    <div class="thumbnail">
+        <a href="">
+						<img src="assets/img/accueil/vedettedeparis.png" alt="" class="thumbnail">
+
+						<div class="surcouche">
+							<div>
+								Une place de tennis offerte pour l'achat d'une place de concert
+							</div>
+						</div>
+					</a>
+        <div class="etiquette">
+            -50% Frère
+        </div>
+        <div class="etiquette_2">
+            -30% Frère
+        </div>
+        <div class="etiquette_3">
+            -30% Frère
+        </div>
+    </div>
+
+    <div class="thumbnail">
+        <a href="">
+						<img src="assets/img/accueil/lost.png" alt="" class="thumbnail">
+
+						<div class="surcouche">
+							<div>
+								Une place de tennis offerte pour l'achat d'une place de concert
+							</div>
+						</div>
+					</a>
+        <div class="etiquette">
+            -50% Frère
+        </div>
+        <div class="etiquette_2">
+            -30% Frère
+        </div>
+        <div class="etiquette_3">
+            -30% Frère
+        </div>
+    </div>
+
+    <div class="thumbnail">
+        <a href="">
+						<img src="assets/img/accueil/kanoak.png" alt="" class="thumbnail">
+
+						<div class="surcouche">
+							<div>
+								Une place de tennis offerte pour l'achat d'une place de concert
+							</div>
+						</div>
+					</a>
+        <div class="etiquette">
+            -50% Frère
+        </div>
+        <div class="etiquette_2">
+            -30% Frère
+        </div>
+        <div class="etiquette_3">
+            -30% Frère
+        </div>
+    </div>
+
+    <div class="thumbnail">
+        <a href="">
+						<img src="assets/img/accueil/zaragoza.png" alt="" class="thumbnail">
+
+						<div class="surcouche">
+							<div>
+								Une place de tennis offerte pour l'achat d'une place de concert
+							</div>
+						</div>
+					</a>
+        <div class="etiquette">
+            -50% Frère
+        </div>
+        <div class="etiquette_2">
+            -30% Frère
+        </div>
+        <div class="etiquette_3">
+            -30% Frère
+        </div>
+    </div>
+
+    <div class="cartepicto">
+
+        <a class="bouton_rose" title="Voir tous les top avantages " href="http://www.cartejeunes.fr/nos-bons-plans/nos-avantages/ ">Voir tous les top avantages</a>
+    </div>
+
+    <div>
+
+        <!---->
+
+        <!--Slider /API Twitter-->
+        <div class="api-twitter">
+
+            <div class="icon-twitter">
+                <a href="https://twitter.com/CarteJeunesFr" target="_blank"><i class="fa fa-twitter"></i></a>
+            </div>
+
+        </div>
+
+        <!---->
+
+
+        <!--Picto-->
+        <h2>LA CARTE JEUNES EUROPÉENNE</h2>
+
+        <div class="cartepicto ">
+            <div class="picto ">
+                <img src="assets/img/obtenir-la-carte/picto-gift.png " class="picto " alt="picto-cadeaux " /><br>
+                <span class="bold sub-picto ">60 000+</span>
+                <br />
+                <span class="bold ">Bons plans pour les jeunes entre 12 et 30 ans</span>
+            </div>
+
+            <div class="picto ">
+                <img src="assets/img/obtenir-la-carte/picto-millions.png " class="picto " alt="picto-membres " /><br>
+                <span class="bold sub-picto ">6 MILLIONS</span>
+                <br />
+                <span class="bold ">De jeunes déjà possesseurs de la carte en Europe</span>
+            </div>
+
+            <div class="picto ">
+                <img src="assets/img/obtenir-la-carte/picto-pays.png " class="picto " alt="picto-pays " /><br>
+                <span class="bold sub-picto ">37 PAYS</span>
+                <br />
+                <span class="bold ">Où la carte est utilisable et ce, quel que soit le pays d’obtention</span>
+            </div>
+
+            <div class="picto ">
+                <img src="assets/img/obtenir-la-carte/picto-annees.png " class="picto " alt="picto-anniversaire " /><br>
+                <span class="bold sub-picto ">29 ANS</span>
+                <br />
+                <span class="bold ">Depuis la création de la EYCA (European Youth Card Association)</span>
+            </div><br>
+
+            <a class="bouton_rose" title="Découvrir la carte " href="http://www.cartejeunes.fr/la-carte ">Découvrir la carte</a>
+        </div>
+
+    </div>
+
+    <div class="clear"></div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/velocity/1.5.0/velocity.js"></script>
+    <script>
+        $(function() {
+            let includes = $('[data-include]');
+            jQuery.each(includes, function() {
+                let file = 'include/' + $(this).data('include') + '.html';
+                let div = this;
+                $.get(file, function(data) {
+                    $(div).replaceWith(data);
+                });
+            });
+        });
+
+    </script>
+    <script src="assets/script/script.js"></script>
+    <script src="assets/script/scrollback.js"></script>
+    <script src="assets/script/slider.js"></script>
+</body>
+
+</html>
---
+++ bons-plans.php
@@ -0,0 +1,360 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width">
+		<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+		<link rel="stylesheet" href="assets/style/style.css">
+		<link rel="stylesheet" href="assets/style/bons_plans.css">
+		<link rel="stylesheet" href="assets/style/slider.css">
+
+	</head>
+	<body>
+		<?php require_once('include/header.html') ?>
+		<?php require_once('include/slider.html') ?>
+		<?php require_once('include/recherche.html') ?>
+		<div class="main bons-plans">
+			
+			<h2>Découvrez nos avantages en France</h2>
+
+			<div class="categories conteneur">
+				<ul class="categories">
+					<li>
+						<a href="">
+							<img src="assets/img/hand.png" alt="">
+							Tous
+						</a>
+					</li><!-- 
+				 --><li>
+						<a href="">
+							<img src="assets/img/hand.png" alt="">
+							Assistance
+						</a>
+					</li><!-- 
+				 --><li>
+						<a href="">
+							<img src="assets/img/hand.png" alt="">
+							Acti city
+						</a>
+					</li><!-- 
+				 --><li>
+						<a href="">
+							<img src="assets/img/hand.png" alt="">
+							Culture
+						</a>
+					</li><!-- 
+				 --><li>
+						<a href="">
+							<img src="assets/img/hand.png" alt="">
+							Education
+						</a>
+					</li><!-- 
+				 --><li>
+						<a href="">
+							<img src="assets/img/hand.png" alt="">
+							Logement
+						</a>
+					</li><!-- 
+				 --><li>
+						<a href="">
+							<img src="assets/img/hand.png" alt="">
+							Loisirs
+						</a>
+					</li><!-- 
+				 --><li>
+						<a href="">
+							<img src="assets/img/hand.png" alt="">
+							Restauration
+						</a>
+					</li><!-- 
+				 --><li>
+						<a href="">
+							<img src="assets/img/hand.png" alt="">
+							Séjours
+						</a>
+					</li><!-- 
+				 --><li>
+						<a href="">
+							<img src="assets/img/hand.png" alt="">
+							Solidarité
+						</a>
+					</li><!-- 
+				 --><li>
+						<a href="">
+							<img src="assets/img/hand.png" alt="">
+							Sport
+						</a>
+					</li><!-- 
+				 --><li>
+						<a href="">
+							<img src="assets/img/hand.png" alt="">
+							Transport
+						</a>
+					</li><!-- 
+				 --><li>
+						<a href="">
+							<img src="assets/img/hand.png" alt="">
+							Coup de coeur
+						</a>
+					</li>
+				</ul>
+				<div class="thumbnail">
+					<a href="">
+						<img src="assets/square/tumb01.jpg" alt="" class="thumbnail">
+
+						<div class="surcouche">
+							<div>
+								Une place de tennis offerte pour l'achat d'une place de concert
+							</div>
+						</div>
+					</a>
+					<div class="etiquette">
+						-50% Frère
+					</div>
+					<div class="etiquette_2">
+						-30% Frère						
+					</div>						
+					<div class="etiquette_3">
+						-30% Frère						
+					</div>
+				</div><!-- 
+			 --><div class="thumbnail">
+					<a href="">
+						<img src="assets/square/tumb02.png" alt="" class="thumbnail">
+						<div class="surcouche">
+							<div>
+								30 % de réduction pour tous vos voyages sur la planète terre
+							</div>
+						</div>
+					</a>
+					<div class="etiquette">
+						-30% Frère
+					</div>
+					<div class="etiquette_2">
+						-30% Frère						
+					</div>						
+					<div class="etiquette_3">
+						-30% Frère						
+					</div>						
+				</div><!-- 
+			 --><div class="thumbnail">
+					<a href="">
+						<img src="assets/square/tumb03.jpg" alt="" class="thumbnail">
+						<div class="surcouche">
+							<div>
+								Diverses réductions sur le trajet vers la nébuleuse du crabe (aller simple)
+							</div>
+						</div>
+					</a>
+					<div class="etiquette">
+						Jusqu'a -60%
+					</div>
+					<div class="etiquette_2">
+						-30% Frère						
+					</div>						
+					<div class="etiquette_3">
+						-30% Frère						
+					</div>						
+				</div><!-- 
+			 --><div class="thumbnail">
+					<a href="">
+						<img src="assets/square/tumb04.jpg" alt="" class="thumbnail">
+						<div class="surcouche">
+							<div>
+								Une place de tennis offerte pour l'achat d'une place de concert
+							</div>
+						</div>
+					</a>
+					<div class="etiquette">
+						-30% Frère
+					</div>
+					<div class="etiquette_2">
+						-30% Frère						
+					</div>						
+					<div class="etiquette_3">
+						-30% Frère						
+					</div>						
+				</div><!-- 
+			 --><div class="thumbnail">
+					<a href="">
+						<img src="assets/square/tumb05.jpg" alt="" class="thumbnail">
+						<div class="surcouche">
+							<div>
+								Une place de tennis offerte pour l'achat d'une place de concert
+							</div>
+						</div>
+					</a>
+					<div class="etiquette">
+						-30% Frère
+					</div>
+					<div class="etiquette_2">
+						-30% Frère						
+					</div>						
+					<div class="etiquette_3">
+						-30% Frère						
+					</div>
+				</div><!-- 
+			 --><div class="thumbnail">
+					<a href="">
+						<img src="assets/square/tumb06.jpg" alt="" class="thumbnail">
+						<div class="surcouche">
+							<div>
+								Une place de tennis offerte pour l'achat d'une place de concert
+							</div>
+						</div>
+					</a>
+					<div class="etiquette">
+						-30% Frère
+					</div>
+					<div class="etiquette_2">
+						-30% Frère						
+					</div>						
+					<div class="etiquette_3">
+						-30% Frère						
+					</div>
+				</div><!-- 
+			 --><div class="thumbnail">
+					<a href="">
+						<img src="assets/square/tumb07.png" alt="" class="thumbnail">
+						<div class="surcouche">
+							<div>
+								Une place de tennis offerte pour l'achat d'une place de concert
+							</div>
+						</div>
+					</a>
+					<div class="etiquette">
+						-30% Frère
+					</div>
+					<div class="etiquette_2">
+						-30% Frère						
+					</div>						
+					<div class="etiquette_3">
+						-30% Frère						
+					</div>
+				</div><!-- 
+			 --><div class="thumbnail">
+					<a href="">
+						<img src="assets/square/tumb08.png" alt="" class="thumbnail">
+						<div class="surcouche">
+							<div>
+								Une place de tennis offerte pour l'achat d'une place de concert
+							</div>
+						</div>
+					</a>
+					<div class="etiquette">
+						-30% Frère
+					</div>
+					<div class="etiquette_2">
+						-30% Frère						
+					</div>						
+					<div class="etiquette_3">
+						-30% Frère						
+					</div>						
+				</div><!-- 
+			 --><div class="thumbnail">
+					<a href="">
+						<img src="assets/square/tumb09.png" alt="" class="thumbnail">
+						<div class="surcouche">
+							<div>
+								Une place de tennis offerte pour l'achat d'une place de concert
+							</div>
+						</div>
+					</a>
+					<div class="etiquette">
+						-30% Frère
+					</div>
+					<div class="etiquette_2">
+						-30% Frère						
+					</div>						
+					<div class="etiquette_3">
+						-30% Frère						
+					</div>						
+				</div><!-- 
+			 --><div class="thumbnail">
+					<a href="">
+						<img src="assets/square/tumb10.png" alt="" class="thumbnail">
+						<div class="surcouche">
+							<div>
+								Une place de tennis offerte pour l'achat d'une place de concert
+							</div>
+						</div>
+					</a>
+					<div class="etiquette">
+						-30% Frère
+					</div>
+					<div class="etiquette_2">
+						-30% Frère						
+					</div>						
+					<div class="etiquette_3">
+						-30% Frère						
+					</div>						
+				</div><!-- 
+			 --><div class="thumbnail">
+					<a href="">
+						<img src="assets/square/tumb01.jpg" alt="" class="thumbnail">
+
+						<div class="surcouche">
+							<div>
+								Une place de tennis offerte pour l'achat d'une place de concert
+							</div>
+						</div>
+					</a>
+					<div class="etiquette">
+						-50% Frère
+					</div>
+					<div class="etiquette_2">
+						-30% Frère						
+					</div>						
+					<div class="etiquette_3">
+						-30% Frère						
+					</div>
+				</div><!-- 
+			 --><div class="thumbnail">
+					<a href="">
+						<img src="assets/square/tumb02.png" alt="" class="thumbnail">
+						<div class="surcouche">
+							<div>
+								30 % de réduction pour tous vos voyages sur la planète terre
+							</div>
+						</div>
+					</a>
+					<div class="etiquette">
+						-30% Frère
+					</div>
+					<div class="etiquette_2">
+						-30% Frère						
+					</div>						
+					<div class="etiquette_3">
+						-30% Frère						
+					</div>						
+				</div><!-- 
+			 --><div class="thumbnail">
+					<a href="">
+						<img src="assets/square/tumb03.jpg" alt="" class="thumbnail">
+						<div class="surcouche">
+							<div>
+								Diverses réductions sur le trajet vers la nébuleuse du crabe (aller simple)
+							</div>
+						</div>
+					</a>
+					<div class="etiquette">
+						Jusqu'a -60%
+					</div>
+					<div class="etiquette_2">
+						-30% Frère						
+					</div>						
+					<div class="etiquette_3">
+						-30% Frère						
+					</div>						
+				</div>
+			</div>
+		</div>
+		<div class="clear"></div>
+		
+		<?php require_once('include/footer.html') ?>
+
+		<script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/velocity/1.5.0/velocity.js"></script>
+		<script src="assets/script/script.js"></script>
+	</body>
+</html>
